9da287f0ac39a60926ce3b083db3d5e7f8844e85
Kai-Cai <kaiaschiff@gmail.com>
2018-06-06 17:43:48 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,38 @@
+# if /3 --> Fizz
+# if /5 --> Buzz
+# if /15 --> FizzBuzz
+
+
+
+# if
+# elsif
+# elsif
+# else
+# end
+
+# number = get.chomp
+# if #{number} /3 == integer 
+#   puts "Fizz"
+  
+  
+  
+# def "fizzbuzz" do
+#   it 'returns "Fizz" when the number is divisible by 3' do
+#     fizz_3 = fizzbuzz(3)
+ 
+#     expect(fizz_3).to eq("Fizz")
+#   end
+# end  
+
+
+it 'returns "Fizz" when the number is divisible by 3' do
+  # Code that actually does what we're testing and confirms the desired result
+end
+
+
+
+# First, get the return value from fizzbuzz(3), store it in a variable, fizz_3
+fizz_3 = fizzbuzz(3)
+ 
+# Next, our expectation that the value of fizz_3 should be "Fizz"
+expect(fizz_3).to eq("Fizz")
