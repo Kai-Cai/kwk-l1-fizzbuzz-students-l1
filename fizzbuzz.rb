bf1e52340595c68b4f2e85ec8ad93376e64467ce
Kai-Cai <kaiaschiff@gmail.com>
2018-06-06 18:16:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,20 +6,20 @@
 puts "What is your number?"
 number = gets.chomp
 
-puts "#{number} is a cool number."
-
-# fizzbuzz(#{number})
 
 
-# def fizzbuzz(number)
-#   if number % 15 == 0
-#   puts "FizzBuzz"
-#   elsif number % 3 == 0
-#   puts "Fizz"
-#   else number % 5 == 0
-#   puts "Buzz"
-# end
-# end
+fizzbuzz(number)
+
+
+def fizzbuzz(number)
+  if number % 15 == 0
+  puts "FizzBuzz"
+  elsif number % 3 == 0
+  puts "Fizz"
+  else number % 5 == 0
+  puts "Buzz"
+end
+end
 
 
 
