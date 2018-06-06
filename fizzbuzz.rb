a6e5f58413148b3ef5eb546664134b15cb6d45c7
Kai-Cai <kaiaschiff@gmail.com>
2018-06-06 19:21:54 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -13,10 +13,10 @@ def fizzbuzz(number)
   puts "FizzBuzz"
   elsif number % 3 == 0
   puts "Fizz"
-  else number % 5 == 0
+  elsif number % 5 == 0
   puts "Buzz"
-# else
-#   puts "nil"
+else
+  puts "nil"
 end
 end
 
