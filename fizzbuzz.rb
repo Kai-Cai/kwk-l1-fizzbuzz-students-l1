a78802bd08c2e586eefd81012b29c272cb5ff59d
Kai-Cai <kaiaschiff@gmail.com>
2018-06-06 18:13:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,6 +3,10 @@
 # if /15 --> FizzBuzz
 
 
+puts "What is your number?"
+number = gets.chomp
+
+fizzbuzz(#{number})
 
 
 def fizzbuzz(number)
@@ -16,38 +20,5 @@ end
 end
 
 
-fizzbuzz(15)
 
 
-# # if
-# # elsif
-# # elsif
-# # else
-# # end
-
-# # number = get.chomp
-# # if #{number} /3 == integer 
-# #   puts "Fizz"
-  
-  
-  
-# # def "fizzbuzz" do
-# #   it 'returns "Fizz" when the number is divisible by 3' do
-# #     fizz_3 = fizzbuzz(3)
- 
-# #     expect(fizz_3).to eq("Fizz")
-# #   end
-# # end  
-
-
-# # it 'returns "Fizz" when the number is divisible by 3' do
-# #   # Code that actually does what we're testing and confirms the desired result
-# # end
-
-
-
-# # First, get the return value from fizzbuzz(3), store it in a variable, fizz_3
-# fizz_3 = fizzbuzz(3)
- 
-# # Next, our expectation that the value of fizz_3 should be "Fizz"
-# expect(fizz_3).to eq("Fizz")
